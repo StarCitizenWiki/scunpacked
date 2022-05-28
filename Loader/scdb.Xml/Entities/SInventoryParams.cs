653a34d02d4130b3df54a3fa8ddcc969606d4bc2
Hannes <hannes@octofox.de>
2022-05-28 18:49:03 +0200

    refactor: Remove duplicate types

--- Loader/scdb.Xml/Entities/SInventoryParams.cs
+++ Loader/scdb.Xml/Entities/SInventoryParams.cs
@@ -5,35 +5,6 @@ namespace scdb.Xml.Entities
 {
 	public class SInventoryParams
 	{
-		[XmlAttribute]
-		public string Type;
-
-		[XmlAttribute]
-		public string SubType;
-
-		[XmlAttribute]
-		public int Size;
-
-		[XmlAttribute]
-		public int Grade;
-
-		[XmlAttribute]
-		public string Manufacturer;
-
-		[XmlAttribute]
-		public string Tags;
-
-		[XmlAttribute]
-		public string RequiredTags;
-
-		[XmlAttribute]
-		public string DisplayType;
-
-		public SCItemLocalization Localization;
-
-		public InventoryOccupancyVolume inventoryOccupancyVolume;
-		public InventoryOccupancyDimensions inventoryOccupancyDimensions;
-
 		public Capacity capacity;
 	}
 }
