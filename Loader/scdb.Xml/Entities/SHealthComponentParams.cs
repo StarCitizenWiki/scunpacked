d77ef5be7e3ac1d0417c1740cf2d692335eca9c9
H. C. Kruse <hannes@octofox.de>
2023-05-10 21:14:30 +0200

    feat: Add more Health params

--- Loader/scdb.Xml/Entities/SHealthComponentParams.cs
+++ Loader/scdb.Xml/Entities/SHealthComponentParams.cs
@@ -1,12 +1,16 @@
-using System.Xml;
 using System.Xml.Serialization;
 
 namespace scdb.Xml.Entities
 {
 	public class SHealthComponentParams
 	{
-		[XmlAttribute]
-		public double Health;
+		[XmlAttribute] public double Health;
+
+		[XmlAttribute] public bool DetachFromItemPortOnDeath;
+		[XmlAttribute] public bool DetachFromEntityOnDeath;
+		[XmlAttribute] public bool PropagateExplosionDamageToChildren;
+		[XmlAttribute] public bool IsSalvagable;
+		[XmlAttribute] public bool IsRepairable;
 
 		public DamageResistances DamageResistances;
 	}
