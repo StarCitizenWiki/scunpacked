fe3791cdc63eaaaeb3665adbbb891b6e61476c04
H. C. Kruse <hannes@octofox.de>
2023-06-27 10:42:58 +0200

    feat: Add initial layer0 parser

---
+++ Loader/scdb.Xml/Layer0/Subsumption.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Xml.Serialization;
+
+namespace scdb.Xml.Layer0
+{
+	public class Subsumption
+	{
+		public Layer Layer;
+	}
+
+	public class Layer
+	{
+		[XmlAttribute]
+		public string ID;
+
+		[XmlAttribute]
+		public string Name;
+
+		public List<Variable> Variables;
+	}
+
+	public class Variable
+	{
+		[XmlAttribute]
+		public string ID;
+
+		[XmlAttribute]
+		public string DefaultValue;
+
+		[XmlAttribute]
+		public string EditorName;
+	}
+
+}
